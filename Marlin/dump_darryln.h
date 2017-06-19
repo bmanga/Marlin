4886f5af1d7360d8e3d62c3f6c28882c43348c47
darryln <embsysdev@gmail.com>
2017-06-18 19:47:12 -0600

    Modified configuration for HICTOP 3DP-11
    Initial commit. Verified basic functionality by printing the Ultimaker
    robot. Motion and heating seem to be fine, although the extruder
    heating takes a while to get up through the last few degrees. Extrude
    seems to gap sometimes, but could be due to air bubbles in the cheap
    filament provided with the kit. SD enabled but init fails; card not
    detected, likely hardware issue with controller or display board.

---
+++ Marlin/dump_darryln.h
@@ -0,0 +1,62 @@
+/*
+
+dump of darryln HDCP-11 settings (this build)
+
+Connecting...
+start
+Printer is now online.
+echo:Marlin 1.1.3
+echo: Last Updated: 2017-06-04 12:00 | Author: (darryln, HICTOP 3DP-11)
+Compiled: Jun 18 2017
+echo: Free Memory: 3558  PlannerBufferBytes: 1232
+echo:Hardcoded Default Settings Loaded
+echo:  G21    ; Units in mm
+echo:  M149 C ; Units in Celsius
+echo:Filament settings: Disabled
+echo:  M200 D3.00
+echo:  M200 D0
+echo:Steps per unit:
+echo:  M92 X80.00 Y80.00 Z398.00 E102.00
+echo:Maximum feedrates (units/s):
+echo:  M203 X200.00 Y200.00 Z3.00 E25.00
+echo:Maximum Acceleration (units/s2):
+echo:  M201 X1000 Y1000 Z100 E10000
+echo:Acceleration (units/s2): P<print_accel> R<retract_accel> T<travel_accel>
+echo:  M204 P3000.00 R3000.00 T3000.00
+echo:Advanced: S<min_feedrate> T<min_travel_feedrate> B<min_segment_time_ms> X<max_xy_jerk> Z<max_z_jerk> E<max_e_jerk>
+echo:  M205 S0.00 T0.00 B20000 X10.00 Y10.00 Z0.40 E5.00
+echo:Home offset:
+echo:  M206 X0.00 Y0.00 Z0.00
+echo:Material heatup parameters:
+echo:  M145 S0 H205 B60 F0
+  M145 S1 H240 B100 F0
+echo:PID settings:
+echo:  M301 P12.19 I0.50 D74.60
+echo:SD init fail
+
+>>> M503
+SENDING:M503
+echo:  G21    ; Units in mm
+echo:  M149 C ; Units in Celsius
+echo:Filament settings: Disabled
+echo:  M200 D3.00
+echo:  M200 D0
+echo:Steps per unit:
+echo:  M92 X80.00 Y80.00 Z398.00 E102.00
+echo:Maximum feedrates (units/s):
+echo:  M203 X200.00 Y200.00 Z3.00 E25.00
+echo:Maximum Acceleration (units/s2):
+echo:  M201 X1000 Y1000 Z100 E10000
+echo:Acceleration (units/s2): P<print_accel> R<retract_accel> T<travel_accel>
+echo:  M204 P3000.00 R3000.00 T3000.00
+echo:Advanced: S<min_feedrate> T<min_travel_feedrate> B<min_segment_time_ms> X<max_xy_jerk> Z<max_z_jerk> E<max_e_jerk>
+echo:  M205 S0.00 T0.00 B20000 X10.00 Y10.00 Z0.40 E5.00
+echo:Home offset:
+echo:  M206 X0.00 Y0.00 Z0.00
+echo:Material heatup parameters:
+echo:  M145 S0 H205 B60 F0
+  M145 S1 H240 B100 F0
+echo:PID settings:
+echo:  M301 P12.19 I0.50 D74.60
+
+*/
---
+++ Marlin/dump_hictop_factory.h
@@ -0,0 +1,44 @@
+/*
+
+DUMP OF FACTORY DEFAULT SETTINGS
+
+echo:Marlin1.0.0
+echo: Last Updated: Jul  1 2016 14:54:40 | Author: (none, default config)
+Compiled: Jul  1 2016
+echo: Free Memory: 3725  PlannerBufferBytes: 1232
+echo:Hardcoded Default Settings Loaded
+echo:Steps per unit:
+echo:  M92 X80.00 Y80.00 Z398.00 E94.50
+echo:Maximum feedrates (mm/s):
+echo:  M203 X500.00 Y500.00 Z5.00 E25.00
+echo:Maximum Acceleration (mm/s2):
+echo:  M201 X9000 Y9000 Z100 E10000
+echo:Acceleration: S=acceleration, T=retract acceleration
+echo:  M204 S3000.00 T3000.00
+echo:Advanced variables: S=Min feedrate (mm/s), T=Min travel feedrate (mm/s), B=minimum segment time (ms), X=maximum XY jerk (mm/s),  Z=maximum Z jerk (mm/s),  E=maximum E jerk (mm/s)
+echo:  M205 S0.00 T0.00 B20000 X20.00 Z0.40 E5.00
+echo:Home offset (mm):
+echo:  M206 X0.00 Y0.00 Z0.00
+echo:PID settings:
+echo:   M301 P22.20 I1.08 D114.00
+
+>>> M503
+SENDING:M503
+echo:Steps per unit:
+echo:  M92 X80.00 Y80.00 Z398.00 E94.50
+echo:Maximum feedrates (mm/s):
+echo:  M203 X500.00 Y500.00 Z5.00 E25.00
+NOTE: extruder max feed rate is ~10 for PLA @ 212C (determined empirically)
+echo:Maximum Acceleration (mm/s2):
+echo:  M201 X9000 Y9000 Z100 E10000
+echo:Acceleration: S=acceleration, T=retract acceleration
+echo:  M204 S3000.00 T3000.00
+echo:Advanced variables: S=Min feedrate (mm/s), T=Min travel feedrate (mm/s), B=minimum segment time (ms), X=maximum XY jerk (mm/s),  Z=maximum Z jerk (mm/s),  E=maximum E jerk (mm/s)
+echo:  M205 S0.00 T0.00 B20000 X20.00 Z0.40 E5.00
+echo:Home offset (mm):
+echo:  M206 X0.00 Y0.00 Z0.00
+echo:PID settings:
+echo:   M301 P22.20 I1.08 D114.00
+
+
+*/
